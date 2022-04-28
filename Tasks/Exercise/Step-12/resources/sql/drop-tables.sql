f88547747467af164b155c63cee959e3ae98880f
Danimo93 <38252161+Danimo93@users.noreply.github.com>
2022-04-28 09:55:23 +0200

    add

--- Tasks/Exercise/Step-12/resources/sql/drop-tables.sql
+++ Tasks/Exercise/Step-12/resources/sql/drop-tables.sql
@@ -1,1 +1,12 @@
-DROP TABLE if exists uusers;
+
+/**
+  Had problem with my table.
+  This syntax is just in case i need to more issues with my data.
+ */
+DROP TABLE if exists users;
+
+
+/**
+ * I had a double name in my table.
+ */
+DELETE FROM users WHERE name='Johan Baumann';
--- Tasks/Exercise/Step-12/resources/sql/sample-insert.sql
+++ Tasks/Exercise/Step-12/resources/sql/sample-insert.sql
@@ -1,4 +1,4 @@
 USE uniUsers;
 
 INSERT INTO users(personnummer, name, dob, campus, school)
-VALUE ('090979', 'Johan Baumann', '1979-09-09', 'OSLO', 'Guess');
+VALUE ('09097990087', 'Johan Baumann', '1979-09-09', 'OSLO', 'Guess');
