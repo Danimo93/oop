913c0585d1ac0b7ca0cbf3f32e7822e4a6c8cafc
Danimo93 <38252161+Danimo93@users.noreply.github.com>
2022-07-15 15:17:52 +0200

    add

--- Parkinglot/src/Main.java
+++ Parkinglot/src/Main.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Cars car = new Cars();
         Database data = new Database();
         Database_Cars dataC = new Database_Questions();
-        BinaryQuiz b = new BinaryQuiz();
+        Cars car = new Cars();
+        Parkinglot park = new Parkinglot();
+        Price pc = new Price();
 
-        
 
     }
 }
