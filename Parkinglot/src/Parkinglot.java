ce69287e335878ad2aa9279d776c67840c3c864f
Danimo93 <38252161+Danimo93@users.noreply.github.com>
2022-07-16 18:09:44 +0200

    add

---
+++ Parkinglot/src/Parkinglot.java
@@ -0,0 +1,12 @@
+import java.sql.SQLException;
+
+public class Parkinglot {
+    public void menu() throws SQLException{
+
+        System.out.println("1. Register Car");
+        System.out.println("2. Unregister Car");
+        System.out.println("3. List Cars");
+        System.out.println("4. List Parkinglot");
+        System.out.println("5. Exit");
+    }
+}
